e020012904806813bd7c597fe232b70d8543b8a3
BrunoSNSantos <techbruno.santos@gmail.com>
2026-01-07 16:45:32 -0300

    feat: criando os arquivos tipo_pet.c e tipo_pet.h e implementando os esqueletos das funções em tempo_pet.h

---
+++ src/tipo_pet.h
@@ -0,0 +1,37 @@
+#ifndef TIPO_PET_H
+#define TIPO_PET_H
+
+#include <stdio.h>
+#define max 10
+
+typedef struct {
+    int codigo;
+    char descricao[max];
+} tipo_pet_dados;
+
+typedef struct tipo_pet_no {
+    tipo_pet_dados data;
+    struct tipo_pet_no *prox;
+    struct tipo_pet_no *ant;
+    struct tipo_pet_no *pai;
+} tipo_pet_no;
+
+typedef struct tipo_pet_lista {
+    tipo_pet_no *cabeca;
+    tipo_pet_no *cauda;
+    int tam;
+} tipo_pet_lista;
+
+tipo_pet_lista* inicar_tipo_pet_lista(void);
+void liberar_tipo_pet_lista(tipo_pet_lista *lista);
+
+int criar_tipo_pet(tipo_pet_lista *lista, tipo_pet_dados dados);
+tipo_pet_dados* buscar_tipo_pet(tipo_pet_lista *lista);
+int atualizar_tipo_pet(tipo_pet_lista *lista, tipo_pet_dados dados);
+int remover_tipo_pet(tipo_pet_lista *lista, tipo_pet_dados dados);
+
+int carregar_tipo_pet_arquivo(tipo_pet_lista *lista, const char *nome_arquivo);
+int salvar_arquivo_tipo_pet(tipo_pet_lista *lista, const char *nome_arquivo);
+
+
+#endif
