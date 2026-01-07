e374a37250420b0c92814f9b39d0337f424bde70
BrunoSNSantos <techbruno.santos@gmail.com>
2026-01-07 17:07:15 -0300

    feat: implementando função liberar_tipo_pet_lista em tipo_pet.c

--- src/tipo_pet.c
+++ src/tipo_pet.c
@@ -11,6 +11,19 @@ tipo_pet_lista* inicar_tipo_pet_lista(void){
     l->cabeca = NULL;
     l->cauda = NULL;
     l->tam = 0;
-    
+
     return 1;
+}
+
+void liberar_tipo_pet_lista(tipo_pet_lista *lista) {
+    if (lista == NULL) return;
+
+    tipo_pet_no *auxp = lista->cabeca;
+    while (auxp != NULL) {
+        tipo_pet_no *no = auxp->prox;
+        free(auxp);
+        auxp = no;
+    }
+    free(lista);
+    return;
 }
